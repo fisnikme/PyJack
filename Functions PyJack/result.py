e82e03ae4b6fa88b815a871891980586442d5714
Andri-2 <andri.schwab@students.fhnw.ch>
2025-11-15 12:32:56 +0000

    .

---
+++ Functions PyJack/result.py
@@ -0,0 +1,16 @@
+blackjack = print("Gratuliere! Blackjack!")
+lost = print("Leider verloren!")
+win = print(Gratuliere, du hast gewonnen!)
+
+
+def lost():
+    print("Leider verloren!")
+    #
+
+
+def win():
+    print("Gratuliere, du hast gewonnen!")
+
+
+def blackjack():
+    print("Gratuliere! Blackjack!")
